9cfa25f5c7deef9813d99e73246db8611fc8816a
Rodolfo Diaz <astonbellz@gmail.com>
2019-05-15 09:19:48 -0400

    primer commit agregado

--- App/phinfo.php
+++ App/phinfo.php
@@ -5,7 +5,7 @@
 <title>Open Project wifi X minuto</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <style>
-<!-- Hide style for old browsers 
+<!-- Hide style for old browsers
 BODY          {font-family: Tahoma;font-size="10"}
 .headd { font-family: Tahoma ; font-size: 13pt; text-decoration:  none; }
 .app { font-family: Tahoma ; font-size: 13pt; text-decoration:  none; }
@@ -20,24 +20,24 @@ A:active  {font-family: Tahoma ; text-decoration: none; color: #FF0000}
 <body bgcolor="#FFFFFF">
 
 <table border="0" width="1200" align="center" height="19" >
-    <tr bgcolor="#D2E9FF"> 
+    <tr bgcolor="#D2E9FF">
       <td width="100%" height="90" align="center" valign="absmiddle"><font color="#000080">
       <span class="headd"><strong><big>&nbsp; #contacto flojos Whatsapp (+58) 0414-9456255# <br>------------------------<font color="#FF0000">------------------------</font></big></strong></span></font></td>
     </tr>
   </table><div align="center"><img src="qr.jpg" width="240"
-    height="240" align="absmiddle"> 
- 
+    height="240" align="absmiddle">
+
   <table width="800" border="0">
-    <tr bgcolor="#F9FBFF"> 
-      <td height="344"> 
-        <blockquote> 
+    <tr bgcolor="#F9FBFF">
+      <td height="344">
+        <blockquote>
           <p><font color="#000080"><span class="headd"><strong><br><img src="members.gif" width="20"
     height="20" align="absmiddle">
                  <span class="app"><a href="https://mail.google.com">Aston bell Email:
 astonbellz@gmail.com</a><br>
-<a href="https://mail.google.com">###Residencias Santa Cruz edo Aragua###</a> <br>
+<a href="">###Residencias Santa Cruz edo Aragua###</a> <br>
 <table border="0" width="1200" align="center" height="19" >
-    <tr> 
+    <tr>
       <td width="100%" height="60" align="center" valign="absmiddle" bgcolor="#D2E9FF">
       <font color="#000080" class="headd">&nbsp;&nbsp;&nbsp;<b>Easy way to Conect, With Qr Code</b> </font></td>
     </tr>
@@ -46,5 +46,3 @@ astonbellz@gmail.com</a><br>
 
 </body>
 </html>
-
-
